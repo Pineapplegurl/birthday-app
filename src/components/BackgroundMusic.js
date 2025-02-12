6f4b31f66b4410ddaf37b70ac2a4a676065c9044
Ines.b <inesberradi@gmail.com>
2025-02-12 21:23:46 +0100

    update

--- src/components/BackgroundMusic.js
+++ src/components/BackgroundMusic.js
@@ -2,18 +2,16 @@ import React, { useRef, useState, useEffect } from 'react';
 
 const BackgroundMusic = () => {
   const audioRef = useRef(null);
-  const [isMuted, setIsMuted] = useState(true); // Muted par défaut
+  const [isMuted, setIsMuted] = useState(true); 
 
   const handleToggleMute = () => {
     if (audioRef.current) {
       if (isMuted) {
-        // Désactive le mode mute et joue la musique
         audioRef.current.muted = false;
         audioRef.current.play().catch((err) => {
           console.error("Erreur lors de la lecture de l'audio :", err);
         });
       } else {
-        // Active le mode mute
         audioRef.current.muted = true;
       }
       setIsMuted(!isMuted);
@@ -22,7 +20,6 @@ const BackgroundMusic = () => {
 
   useEffect(() => {
     if (audioRef.current) {
-      // Met la musique en sourdine par défaut
       audioRef.current.muted = true;
     }
   }, []);
